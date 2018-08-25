9b8e5c8ca87299b0b592dd392e4a52dca82d95f2
whd <1374702658@qq.com>
2018-08-25 14:39:09 +0800

    注册

--- src/api/php/login.php
+++ src/api/php/login.php
@@ -1,14 +1,20 @@
-<?php 
-    include "dbhelper.php"; 
-    $username = !isset($_POST["username"]) ? "" : $_POST["username"];
-    $password = !isset($_POST["password"]) ? "" : $_POST["password"];
-
-    $sql = "select * from login where username = '$username' and password = '$password'";
-
+<?php
+    
+     session_start();
+    include 'dbhelper.php';
+    // 接收前端post传过来的数据
+    $username = isset($_POST['username']) ? $_POST['username'] : "";
+    $password = isset($_POST['password']) ? $_POST['password'] : "";
+    echo "$username,$password";
+    $sql = "select * from users where username = '" . $username . "' and password = '" . $password . "'";
     $result = query_sql($sql);
     if(count($result) > 0){
-        echo "{status: true}";
+        //使用session保存登录信息
+        $_SESSION['user'] = $username;
+        echo "{status: true}"; 
     } else {
         echo "{status: false, message: '登录失败'}";
     }
+
+
 ?>
--- src/js/reg.js
+++ src/js/reg.js
@@ -1,183 +1,73 @@
-document.addEventListener('DOMContentLoaded',()=>{
-
-    let username = document.querySelector('.username');
-    let password = document.querySelector('.password');
-    let btnReg = document.querySelector('.btn');
-    let no = document.querySelector('.no');
-    let no2 = document.querySelector('.no2');
-    let no3 = document.querySelector('.no3');
-    let code = document.querySelector('.mycode');
-    let showCode = document.querySelector('.showcode');
-    
-    // let form = document.querySelector('.form');
-
+(function(){
+    let username = document.querySelector('#username');
+    let yz = document.querySelector('.yz');
+    let password = document.querySelector('#password');
+    let btn = document.querySelector('#btn');
+    console.log(btn);
     let status = [200,304];
-
 
     let isok = false;
 
-    username.focus();
-        //随机颜色验证码
-        function cs(min,max,n){   
-            var code = '';
-            var mima='';
-            for(var i = 0; i<n;i++){
-                var r = parseInt(Math.random()*256);
-                var g = parseInt(Math.random()*256);
-                var b = parseInt(Math.random()*256);
-                var res = parseInt(Math.random()*(max-min+1)+min);
-                mima +=res;
-                code+= '<span style="color:'+'rgb('+r+','+g+','+b+')'+'">'+res+'</span>' ;  
-
-                
-    }
-    return [code,mima];
-}
-
-
-        var sc = cs(0,9,4);
-
-       showCode.onclick = function(){
-            sc = cs(0,9,4);
-            showCode.innerHTML = sc[0];
-          
-       }
-   
-
-
-    
-        showCode.innerHTML= sc[0];
-        var aa= showCode.innerHTML;
-        console.log(aa)
-
-        code.onfocus = function(){
-             var timest = setInterval(function yzm(){
-                     var _code = code.value;
-                    
-                    if(_code ==Number(sc[1])){
-                       
-                        no3.style.display = 'none';
-                        clearInterval(timest)
-                        console.log(_code)
-                          return;    
-                        
-                    }else{
-                        no3.style.display = 'block';
-                       
-                        // console.log(_code);
-                        
-                         return false;
-                    }
-                    
-                },1000);
-        }
-
-        
-       
-          
-    
-        
-        //获取用户名/密码
-        onkeyup = function(e){
-
-            var key = e.keyCode || e.which; 
-            
-            let _username = username.value;
-            let _password = password.value;
-            let _code = code.value;
-         
-            if(!/^[1-9][a-z0-9_\-]{5,19}$/i.test(_username)){
-                no.style.display = 'block';
-                
-                return false;
-            }else{
-                no.style.display = 'none';
-            }
-           
-            if(!/^[^\s]{6,16}$/i.test(_password)){
-
-                no2.style.display = 'block';
-               
-                return false;
-            }else{
-                no2.style.display = 'none';
-            }
-
-            let repassword = document.querySelector('.repassword').value;
-            let ckp = document.querySelector('.ckp');
-            if(repassword != password){
-                // alert('两次输入密码不一致');
-                ckp.innerText="两次输入密码不一致";
-                return false;
-            }
-            else{
-                ckp.innerText="";
-                return true;
-            }
-        
-        
-             let checkbox = document.querySelector('#checkbox').checked;
-             if(checkbox != true){
-                 alert('请阅读交易条款');
-                 return false;
-            }
-        }
-        // console.log(res)
-
-        btnReg.onclick = ()=>{
-
-            console.log(isok)
+    // 注册
+    btn.onclick = function(){
         if(!isok){
             return false;
         }
+        
+        // 获取用户名/密码
         let _username = username.value;
-
-        let _password = password.value;
-
-        console.log( _password);
-
+        var _password = password.value;
+        if(_username ==''||_password ==''){
+            return false;
+        }
         let xhr = new XMLHttpRequest();
         xhr.onload = ()=>{
             if(status.indexOf(xhr.status) >= 0){
-                console.log(xhr.responseText);location.href = '../index.html';
+                console.log(xhr.responseText);
+                if(xhr.responseText === 'success'){
+                    //跳转到登录页面
+                    location.href = "../html/login.html";
+                }
+              
             }
         }
         xhr.open('post','../api/php/register.php',true);
 
-        //设置请求头，以便后端接收post数据
+        // 设置请求头，以便后端接收post数据
         xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
 
+
         xhr.send(`username=${_username}&password=${_password}`);
-        
     }
 
-    //验证用户名是否存在
-    username.onblur =()=>{
+
+    // 验证用户名是否存在
+    username.onblur = ()=>{
+        password.value = '';
         let _username = username.value;
 
         let xhr = new XMLHttpRequest();
-   
+        
         xhr.onload = ()=>{
-           
-            if(status.indexOf(xhr.status) >= 0){ 
-                
-                var aa = xhr.responseText
-                console.log(aa)
-                 
-                if(aa == 'yes'){
-                    console.log(666)
+            if(status.indexOf(xhr.status) >= 0){
+                var formGroup = username.parentNode;
+                if(xhr.responseText === 'yes'){
                     isok = true;
-                }else if(xhr.responseText == 'no'){
-                    console.log(7777)
+
+                    // 成功：添加has-success类
+                    formGroup.classList.remove('has-error');
+                    formGroup.classList.add('has-success');
+                   yz.innerText = '';
+                }else if(xhr.responseText === 'no'){
                     isok = false;
-                  
-                    username.nextElementSibling.innerText = '用户名已存在';
-                    username.focus();
-                } 
+                    formGroup.classList.remove('has-success');
+                    formGroup.classList.add('has-error');
+                   yz.innerText = '用户名已存在';
+                }
             }
         }
-       
         xhr.open('get','../api/php/check_username.php?username='+_username,true);
         xhr.send();
-    } 
-});
+
+    }
+})();
